35355ed934a1e6cf0b28a4afadc826b1ec4554d2
Ian Fleming <iantfleming1992@gmail.com>
2020-11-30 15:20:42 +0000

    respond to balance

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,3 +1,12 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  it 'can create an instance of airport' do
+    expect(subject).to be_kind_of(Oystercard)
+  end
+
+  it 'responds to the method balance' do
+    expect(subject).to respond_to(:balance)
+  end
+
 end
